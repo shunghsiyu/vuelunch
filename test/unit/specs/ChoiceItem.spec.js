1dfb3a16b2e2f03e53a6c24c5b936720288999b4
Shung-Hsi Yu <shunghsiyu@gmail.com>
2017-03-09 08:14:50 +0000

    Update unit test for ChoiceItem
    * Use Sinon's Sandbox feature to make restoring easier.
    * Stub ChoiceItem's $parent property.
    * Add testcase against the checkbox element and restaurant-name element.

--- test/unit/specs/ChoiceItem.spec.js
+++ test/unit/specs/ChoiceItem.spec.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 const ChoiceItemInjector = require('!!vue-loader?inject!@/components/ChoiceItem')
 
 describe('ChoiceItem.vue', function () {
+  let sandbox = null
   let spy = null
   let ChoiceItem = null
   let vm = null
 
   beforeEach(function () {
-    spy = sinon.spy()
+    sandbox = sinon.sandbox.create()
+    spy = sandbox.spy()
     // Mock the API call
     ChoiceItem = ChoiceItemInjector({
       '../api': { simpleApi: spy }
@@ -15,6 +17,13 @@ describe('ChoiceItem.vue', function () {
     const Constructor = Vue.extend(ChoiceItem)
     vm = new Constructor()
     vm.$mount()
+    vm.$parent = {
+      votable: sandbox.stub()
+    }
+  })
+
+  afterEach(() => {
+    sandbox.restore()
   })
 
   it('should have called simpleApi', function () {
@@ -34,6 +43,49 @@ describe('ChoiceItem.vue', function () {
     Vue.nextTick(() => {
       expect(nameElement.textContent).to.equal(name)
       done()
+    })
+  })
+
+  describe('Restaurant Name', function () {
+    let nameElement = null
+    let buttonElement = null
+
+    beforeEach(() => {
+      nameElement = vm.$el.querySelector('.restaurant-name')
+      buttonElement = vm.$el.querySelector('input[type="checkbox"]')
+      sandbox.stub(buttonElement, 'click')
+    })
+    it('should forward click to Checkbox', function () {
+      nameElement.click()
+      expect(buttonElement.click).to.have.been.called
+    })
+  })
+
+  describe('Checkbox', function () {
+    let buttonElement = null
+
+    beforeEach(() => {
+      buttonElement = vm.$el.querySelector('input[type="checkbox"]')
+      // Prevent alert from showing
+      sandbox.stub(window, 'alert')
+    })
+
+    it('should become checked when clicked if parent is votable', function (done) {
+      vm.$parent.votable.returns(true)
+      buttonElement.click()
+      Vue.nextTick(() => {
+        expect(buttonElement.checked).to.be.ok
+        done()
+      })
+    })
+
+    it('should not become checked when clicked if parent is not votable', function (done) {
+      vm.$parent.votable.returns(false)
+      buttonElement.click()
+      Vue.nextTick(() => {
+        expect(buttonElement.checked).to.be.not.ok
+        done()
+      })
     })
   })
 })
